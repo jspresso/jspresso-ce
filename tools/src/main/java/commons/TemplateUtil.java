3909805eedec7c8be7254e69d52a1fdac065ee29
Vincent Vandenschrick <vvandens@jspresso.org>
2006-12-04 10:12:03 +0000

    Resources remote update.

---
+++ tools/src/main/java/commons/TemplateUtil.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2005 Design2see. All rights reserved.
+ */
+package commons;
+
+import java.util.Date;
+
+/**
+ * This is a helper class for freemarker template engine.
+ */
+public class TemplateUtil {
+
+  /**
+   * Returns the current date.
+   *
+   * @return the current date.
+   */
+  public Date getDateNow() {
+    return new Date();
+  }
+
+}
