17b1629cdf57d417405fd61ba868af6f6e4850d1
Vincent Vandenschrick <vvandens@jspresso.org>
2006-12-21 18:16:22 +0000

    Script template

--- app-launch/src/main/java/com/d2s/framework/application/launch/ulc/ExtendedFileService.java
+++ app-launch/src/main/java/com/d2s/framework/application/launch/ulc/ExtendedFileService.java
@@ -24,6 +24,95 @@ import com.ulcjava.base.trusted.AllPermissionsFileService;
  */
 public class ExtendedFileService extends AllPermissionsFileService {
 
+  // private static JFileChooser createFileChooser(
+  // FileChooserConfig fileChooserConfig) {
+  // JFileChooser fileChooser = new JFileChooser();
+  //
+  // if (fileChooserConfig.getSelectedFile() != null) {
+  // File file = new File(fileChooserConfig.getSelectedFile());
+  // fileChooser.setSelectedFile(file);
+  // }
+  // if (fileChooserConfig.getCurrentDirectory() != null) {
+  // File dir = new File(fileChooserConfig.getCurrentDirectory());
+  // fileChooser.setCurrentDirectory(dir);
+  // }
+  // if (fileChooserConfig.getDialogTitle() != null) {
+  // fileChooser.setDialogTitle(fileChooserConfig.getDialogTitle());
+  // }
+  // if (fileChooserConfig.getApproveButtonToolTipText() != null) {
+  // fileChooser.setApproveButtonToolTipText(fileChooserConfig
+  // .getApproveButtonToolTipText());
+  // }
+  // if (fileChooserConfig.getApproveButtonMnemonic() != 0) {
+  // fileChooser.setApproveButtonMnemonic(fileChooserConfig
+  // .getApproveButtonMnemonic());
+  // }
+  // fileChooser.setFileHidingEnabled(fileChooserConfig.isFileHidingEnabled());
+  // fileChooser.setFileSelectionMode(fileChooserConfig.getFileSelectionMode());
+  // fileChooser.setDialogType(fileChooserConfig.getDialogType());
+  // fileChooser.setAcceptAllFileFilterUsed(fileChooserConfig
+  // .isAcceptAllFileFilterUsed());
+  //
+  // for (int i = 0; i < fileChooserConfig.getFileFilterConfigs().length; i++) {
+  // FileChooserConfig.FileFilterConfig fileFilterConfig = fileChooserConfig
+  // .getFileFilterConfigs()[i];
+  //
+  // final String[] allowedExtensions = fileFilterConfig
+  // .getAllowedExtensions();
+  // final String description = fileFilterConfig.getDescription();
+  // FileFilter fileFilter = new FileFilter() {
+  //
+  // @Override
+  // public boolean accept(File file) {
+  // if (file.isDirectory()) {
+  // return true;
+  // }
+  // for (int j = 0; j < allowedExtensions.length; j++) {
+  // if (file.getName().endsWith(allowedExtensions[j])) {
+  // return true;
+  // }
+  // }
+  // return false;
+  // }
+  //
+  // @Override
+  // public String getDescription() {
+  // return description;
+  // }
+  // };
+  //
+  // fileChooser.addChoosableFileFilter(fileFilter);
+  // }
+  //
+  // fileChooser.setMultiSelectionEnabled(false);
+  // fileChooser.putClientProperty("FileChooser.useShellFolder", Boolean.FALSE);
+  //
+  // return fileChooser;
+  // }
+  //
+  // private String baseChooseFile(final FileChooserConfig fileChooserConfig) {
+  // String chosenFile = AccessController
+  // .doPrivileged(new PrivilegedAction<String>() {
+  //
+  // public String run() {
+  // JFileChooser fileChooser = createFileChooser(fileChooserConfig);
+  //
+  // int fileChooserResult = fileChooser.showDialog(null,
+  // fileChooserConfig.getApproveButtonText());
+  //
+  // switch (fileChooserResult) {
+  // case JFileChooser.APPROVE_OPTION:
+  // return fileChooser.getSelectedFile().getAbsolutePath();
+  // case JFileChooser.CANCEL_OPTION:
+  // case JFileChooser.ERROR_OPTION:
+  // default:
+  // return null;
+  // }
+  // }
+  // });
+  // return chosenFile;
+  // }
+
   /**
    * {@inheritDoc}
    */
