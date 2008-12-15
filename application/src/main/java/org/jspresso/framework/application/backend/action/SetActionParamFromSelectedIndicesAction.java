545099819f29f7160136173ed81035d018d7fa0f
Vincent Vandenschrick <vvandens@jspresso.org>
2008-12-15 19:38:46 +0000

    Added a simple action to bind the selected components in the action context.

---
+++ application/src/main/java/org/jspresso/framework/application/backend/action/SetActionParamFromSelectedIndicesAction.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2005-2008 Vincent Vandenschrick. All rights reserved.
+ *
+ *  This file is part of the Jspresso framework.
+ *
+ *  Jspresso is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Jspresso is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Jspresso.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jspresso.framework.application.backend.action;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+import org.jspresso.framework.action.ActionContextConstants;
+import org.jspresso.framework.action.IActionHandler;
+import org.jspresso.framework.binding.ICollectionConnector;
+
+/**
+ * A trivial backend action that updates the action context by setting the
+ * ActionContextConstants.ACTION_PARAM with the selected components of the
+ * backend connector.
+ * <p>
+ * Copyright (c) 2005-2008 Vincent Vandenschrick. All rights reserved.
+ * <p>
+ * This file is part of the Jspresso framework. Jspresso is free software: you
+ * can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version. Jspresso is
+ * distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details. You should have received a copy of the GNU Lesser General Public
+ * License along with Jspresso. If not, see <http://www.gnu.org/licenses/>.
+ * <p>
+ * 
+ * @version $LastChangedRevision$
+ * @author Vincent Vandenschrick
+ */
+public class SetActionParamFromSelectedIndicesAction extends
+    AbstractCollectionAction {
+
+  /**
+   * Sets the ActionContextConstants.ACTION_PARAM with the selected components
+   * of the backend connector.
+   * <p>
+   * {@inheritDoc}
+   */
+  @Override
+  public boolean execute(IActionHandler actionHandler,
+      Map<String, Object> context) {
+    int[] selectedIndices = getSelectedIndices(context);
+    ICollectionConnector collectionConnector = getModelConnector(context);
+    if (selectedIndices == null || selectedIndices.length == 0
+        || collectionConnector == null) {
+      return true;
+    }
+    Collection<Object> selectedComponents = new ArrayList<Object>();
+    for (int i = 0; i < selectedIndices.length; i++) {
+      Object element = collectionConnector
+          .getChildConnector(selectedIndices[i]).getConnectorValue();
+      selectedComponents.add(element);
+    }
+    context.put(ActionContextConstants.ACTION_PARAM, selectedComponents);
+    return super.execute(actionHandler, context);
+  }
+}
