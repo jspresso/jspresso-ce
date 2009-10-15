d4bac058e6469460bbdc84e18e28f02f6ae8de91
Vincent Vandenschrick <vvandens@jspresso.org>
2009-10-15 17:39:18 +0000

    Added groovy source capacity

--- archetype/src/main/resources/archetype-resources/core/src/main/dsl/model.groovy
+++ archetype/src/main/resources/archetype-resources/core/src/main/dsl/model.groovy
@@ -1,10 +1,11 @@
 import org.jspresso.contrib.sjs.domain.Domain;
 
 def domainBuilder = new Domain()
-domainBuilder.Project('${parentArtifactId}',
-  namespace:'${package}'){
-    Domain{
+domainBuilder.Project('${parentArtifactId}') {
+  namespace('${package}') {
+    Domain {
       // Implement your domain here using the SJS DSL.
     }
   }
+}
 domainBuilder.writeDomainFile(project.properties['outputDir'],project.properties['outputFileName'])
