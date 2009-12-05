1de2d746276d6837f3b6ce2d149d98611099f680
Vincent Vandenschrick <vvandens@jspresso.org>
2009-12-05 15:36:51 +0000

    remove getType method on AbstractComponentExtension

--- model/src/main/java/org/jspresso/framework/model/component/AbstractComponentExtension.java
+++ model/src/main/java/org/jspresso/framework/model/component/AbstractComponentExtension.java
@@ -23,17 +23,15 @@ import java.beans.PropertyChangeListener;
 
 import org.jspresso.framework.util.bean.IPropertyChangeCapable;
 
-
 /**
  * This abstract class is a helper base class for components extensions.
- * Developpers should inherit from it and use the <code>getComponent()</code>
- * to access the extended component instance.
+ * Developpers should inherit from it and use the <code>getComponent()</code> to
+ * access the extended component instance.
  * 
  * @version $LastChangedRevision$
  * @author Vincent Vandenschrick
  * @param <T>
- *            the parametrized component class on which these extensions work
- *            on.
+ *          the parametrized component class on which these extensions work on.
  */
 public abstract class AbstractComponentExtension<T extends IComponent>
     implements IComponentExtension<T>, IComponentFactoryAware {
@@ -45,7 +43,7 @@ public abstract class AbstractComponentExtension<T extends IComponent>
    * Constructs a new <code>AbstractComponentExtension</code> instance.
    * 
    * @param component
-   *            The extended component instance.
+   *          The extended component instance.
    */
   public AbstractComponentExtension(T component) {
     this.extendedComponent = component;
@@ -59,19 +57,10 @@ public abstract class AbstractComponentExtension<T extends IComponent>
   }
 
   /**
-   * Computes the entity type.
-   * 
-   * @return The entity type.
-   */
-  public String getType() {
-    return getComponent().getComponentContract().getName();
-  }
-
-  /**
    * Sets the componentFactory.
    * 
    * @param componentFactory
-   *            the componentFactory to set.
+   *          the componentFactory to set.
    */
   public void setComponentFactory(IComponentFactory componentFactory) {
     this.componentFactory = componentFactory;
@@ -90,11 +79,11 @@ public abstract class AbstractComponentExtension<T extends IComponent>
    * Registers a property change listener to forward property changes.
    * 
    * @param sourceBean
-   *            the source bean.
+   *          the source bean.
    * @param sourceProperty
-   *            the name of the source property.
+   *          the name of the source property.
    * @param forwardedProperty
-   *            the name of the forwarded property.
+   *          the name of the forwarded property.
    */
   protected void registerNotificationForwarding(
       IPropertyChangeCapable sourceBean, String sourceProperty,
