50b4ae942b19004bbfeae361fd008d237674893b
Vincent Vandenschrick <vvandens@jspresso.org>
2010-03-19 14:46:14 +0000

    Year copyright -> 2010

--- util/src/main/java/org/jspresso/framework/util/freemarker/TemplateUtil.java
+++ util/src/main/java/org/jspresso/framework/util/freemarker/TemplateUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005-2009 Vincent Vandenschrick. All rights reserved.
+ * Copyright (c) 2005-2010 Vincent Vandenschrick. All rights reserved.
  */
 package org.jspresso.framework.util.freemarker;
 
