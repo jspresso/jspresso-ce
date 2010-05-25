fbaa0417f13234a9f4441d8f4fdc55960b6d5cc9
Vincent Vandenschrick <vvandens@jspresso.org>
2010-05-25 14:06:38 +0000

    Ref doc improved

--- archetype/src/main/resources/archetype-resources/core/src/main/dsl/frontend.groovy
+++ archetype/src/main/resources/archetype-resources/core/src/main/dsl/frontend.groovy
@@ -1,5 +1,11 @@
 // Implement your application frontend here using the SJS DSL.
 
+/*
+ * The workspaces and modules
+ */
+
+// Describe your workspaces and modules here.
+
 controller '${parentArtifactId}.name',
   icon:'icon.png',
   context:'${parentArtifactId}',
