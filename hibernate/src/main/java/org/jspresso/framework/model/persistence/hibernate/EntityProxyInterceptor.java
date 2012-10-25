c22bc904cfdff880ca722bafea729706da985070
Vincent Vandenschrick <vvandens@jspresso.org>
2012-10-25 10:40:57 +0000

    Make sure that a proxy is always considered not transient without getting its contract

--- hibernate/src/main/java/org/jspresso/framework/model/persistence/hibernate/EntityProxyInterceptor.java
+++ hibernate/src/main/java/org/jspresso/framework/model/persistence/hibernate/EntityProxyInterceptor.java
@@ -23,6 +23,7 @@ import java.util.Collection;
 
 import org.hibernate.EmptyInterceptor;
 import org.hibernate.EntityMode;
+import org.hibernate.Hibernate;
 import org.hibernate.type.Type;
 import org.jspresso.framework.model.component.ILifecycleCapable;
 import org.jspresso.framework.model.entity.EntityException;
@@ -107,6 +108,17 @@ public class EntityProxyInterceptor extends EmptyInterceptor {
       }
     }
     return stateUpdated;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Boolean isTransient(Object entity) {
+    if (!Hibernate.isInitialized(entity)) {
+      return Boolean.FALSE;
+    }
+    return super.isTransient(entity);
   }
 
   /**
