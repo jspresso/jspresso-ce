4de95539d059b1b95369236c772f9b20c620c68b
Vincent Vandenschrick <vvandens@jspresso.org>
2013-02-25 11:08:53 +0000

    Maven plugins as Jspresso modules

---
+++ maven/entitygenerator-maven-plugin/src/main/java/org/jspresso/maven/EntityGeneratorMojo.java
@@ -0,0 +1,241 @@
+package org.jspresso.maven;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.io.File;
+import java.net.URL;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.classworlds.ClassRealm;
+import org.codehaus.classworlds.ClassWorld;
+import org.jspresso.framework.tools.entitygenerator.EntityGenerator;
+import org.slf4j.impl.StaticLoggerBinder;
+
+/**
+ * Goal which generates entities for a Jspresso project.
+ */
+@Mojo(name = "generate-entities", defaultPhase = LifecyclePhase.GENERATE_SOURCES, requiresDependencyResolution = ResolutionScope.COMPILE)
+public class EntityGeneratorMojo extends AbstractMojo {
+
+  /**
+   * The Maven project.
+   */
+  @Parameter(defaultValue = "${project}", required = true, readonly = true)
+  MavenProject     project;
+
+  /**
+   * The source directory containing dsl files needed for change detection.
+   */
+  @Parameter(required = false)
+  private File[]   watchedSources;
+
+  /**
+   * Uses given selector too lookup the bean ref factory context file. If not
+   * set, defaults to beanRefFactory.xml.
+   */
+  @Parameter(required = true)
+  private String   beanFactorySelector;
+
+  /**
+   * Uses given applicationContextKey as registered in the spring
+   * BeanFactoryLocator.
+   */
+  @Parameter(required = true)
+  private String   applicationContextKey;
+
+  /**
+   * Generates code for the given component descriptor identifiers in the
+   * application context.
+   */
+  @Parameter(required = false)
+  private String[] componentIds;
+
+  /**
+   * Excludes classes whose names match the regular expression.
+   */
+  @Parameter(required = false)
+  private String[] excludePatterns;
+
+  /**
+   * Generates java5 annotations (incompatible with XDoclet as of now).
+   */
+  @Parameter(defaultValue = "false", required = false)
+  private boolean  generateAnnotations;
+
+  /**
+   * Generates code for the component descriptors declared in the listed
+   * packages.
+   */
+  @Parameter(property = "includePackages", required = false)
+  private String[] includePackages;
+
+  /**
+   * Sets the output directory for generated source.
+   */
+  @Parameter(defaultValue = "${project.build.directory}/generated-sources/entitygenerator", required = true)
+  private File     outputDir;
+
+  /**
+   * Sets the file extension for generated source.
+   */
+  @Parameter(defaultValue = "java", required = true)
+  private String   fileExtension;
+
+  /**
+   * Prepends a prefix to generated class names.
+   */
+  @Parameter(required = false)
+  private String   classnamePrefix;
+
+  /**
+   * Appends a suffix to generated class names.
+   */
+  @Parameter(required = false)
+  private String   classnameSuffix;
+
+  /**
+   * Sets the used component code template.
+   */
+  @Parameter(defaultValue = "HibernateXdocletEntity.ftl", required = true)
+  private String   templateName;
+
+  /**
+   * Sets the path to lookup the template from.
+   */
+  @Parameter(defaultValue = "/org/jspresso/framework/tools/entitygenerator", required = true)
+  private String   templateResourcePath;
+
+  /**
+   * Triggers thee execution of EntityGenerator.
+   * <p>
+   * {@inheritDoc}
+   */
+  @Override
+  public void execute() throws MojoExecutionException {
+    if (watchedSources == null) {
+      watchedSources = new File[] {
+          new File(project.getBasedir(), "src/main/dsl"),
+          new File(project.getBasedir(), "src/main/resources")
+      };
+    }
+    // bind slf4j to maven log
+    StaticLoggerBinder.getSingleton().setLog(getLog());
+    if (isChangeDetected()) {
+      setupPluginClasspath();
+      runEntityGenerator();
+    } else {
+      getLog().info("No change detected. Skipping generation.");
+    }
+    project.addCompileSourceRoot(outputDir.getPath());
+  }
+
+  private void runEntityGenerator() {
+    EntityGenerator generator = new EntityGenerator();
+    generator.setBeanFactorySelector(beanFactorySelector);
+    generator.setApplicationContextKey(applicationContextKey);
+    generator.setTemplateResourcePath(templateResourcePath);
+    generator.setTemplateName(templateName);
+    generator.setOutputDir(outputDir.getAbsolutePath());
+    generator.setFileExtension(fileExtension);
+    generator.setClassnamePrefix(classnamePrefix);
+    generator.setClassnameSuffix(classnameSuffix);
+    generator.setIncludePackages(includePackages);
+    generator.setExcludePatterns(excludePatterns);
+    generator.setGenerateAnnotations(generateAnnotations);
+    generator.setComponentIds(componentIds);
+    generator.generateComponents();
+  }
+
+  private boolean isChangeDetected() {
+    if (!outputDir.exists()) {
+      return true;
+    }
+    long outputLastModified = latestModified(outputDir,
+        outputDir.lastModified());
+    for (File watchedSource : watchedSources) {
+      getLog()
+          .info("Scanning for changes : " + watchedSource.getAbsolutePath());
+      if (hasChangedModelDslFile(watchedSource, outputLastModified)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private long latestModified(File root, long maxLastModified) {
+    long latest = maxLastModified;
+    if (root.lastModified() > maxLastModified) {
+      latest = root.lastModified();
+    }
+    if (root.isDirectory()) {
+      for (File child : root.listFiles()) {
+        latest = latestModified(child, latest);
+      }
+    }
+    return latest;
+  }
+
+  private boolean hasChangedModelDslFile(File source, long maxLastModified) {
+    if (source.isDirectory()) {
+      for (File childSource : source.listFiles()) {
+        if (hasChangedModelDslFile(childSource, maxLastModified)) {
+          return true;
+        }
+      }
+    } else if (source.getName().toLowerCase().indexOf("model") >= 0) {
+      if (source.lastModified() > maxLastModified) {
+        getLog().info(
+            "Detected a change on resource " + source.toString() + ". "
+                + new Date(source.lastModified()) + " > "
+                + new Date(maxLastModified));
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private void setupPluginClasspath() throws MojoExecutionException {
+    ClassWorld world = new ClassWorld();
+    ClassRealm realm;
+    try {
+      realm = world.newRealm("maven.plugin." + getClass().getSimpleName(),
+          Thread.currentThread().getContextClassLoader());
+      List<String> compileClasspathElements = project
+          .getCompileClasspathElements();
+      for (String element : compileClasspathElements) {
+        File elementFile = new File(element);
+        getLog().debug(
+            "Adding element to plugin classpath " + elementFile.getPath());
+        URL url = new URL("file:///" + elementFile.getPath()
+            + (elementFile.isDirectory() ? "/" : ""));
+        realm.addConstituent(url);
+      }
+    } catch (Exception ex) {
+      throw new MojoExecutionException(ex.toString(), ex);
+    }
+    Thread.currentThread().setContextClassLoader(realm.getClassLoader());
+  }
+
+}
---
+++ maven/sjs-maven-plugin/src/main/java/org/jspresso/maven/SjsMojo.java
@@ -0,0 +1,181 @@
+package org.jspresso.maven;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import groovy.lang.Binding;
+import groovy.util.GroovyScriptEngine;
+import groovy.util.ResourceException;
+import groovy.util.ScriptException;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Date;
+import java.util.Properties;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.slf4j.impl.StaticLoggerBinder;
+
+/**
+ * Goal which performs SJS compilation for a Jspresso project.
+ */
+@Mojo(name = "compile-sjs", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
+public class SjsMojo extends AbstractMojo {
+
+  /**
+   * The Maven project.
+   */
+  @Parameter(defaultValue = "${project}", required = true, readonly = true)
+  MavenProject   project;
+
+  /**
+   * The directory containing the grouvy dsl sources.
+   */
+  @Parameter(defaultValue = "${basedir}/src/main/dsl", required = true)
+  private File   srcDir;
+
+  /**
+   * The name of the application groovy file that packs the application.
+   */
+  @Parameter(defaultValue = "application.groovy", required = true)
+  private String applicationFileName;
+
+  /**
+   * Sets the output directory for generated resources.
+   */
+  @Parameter(defaultValue = "${project.build.directory}/generated-resources/dsl", required = true)
+  private File   outputDir;
+
+  /**
+   * The target file name for the model Spring beans.
+   */
+  @Parameter(defaultValue = "dsl-model.xml", required = true)
+  private String modelOutputFileName;
+
+  /**
+   * The target file name for the view Spring beans.
+   */
+  @Parameter(defaultValue = "dsl-view.xml", required = true)
+  private String viewOutputFileName;
+
+  /**
+   * The target file name for the backend Spring beans.
+   */
+  @Parameter(defaultValue = "dsl-back.xml", required = true)
+  private String backOutputFileName;
+
+  /**
+   * The target file name for the frontend Spring beans.
+   */
+  @Parameter(defaultValue = "dsl-front.xml", required = true)
+  private String frontOutputFileName;
+
+  /**
+   * Triggers thee execution of SJS compilation.
+   * <p>
+   * {@inheritDoc}
+   */
+  @Override
+  public void execute() throws MojoExecutionException {
+    // bind slf4j to maven log
+    StaticLoggerBinder.getSingleton().setLog(getLog());
+    if (isChangeDetected()) {
+      try {
+        runSjsCompilation();
+      } catch (IOException ex) {
+        new MojoExecutionException(
+            "An unexpected exception occured when running SJS compilation.", ex);
+      } catch (ResourceException ex) {
+        new MojoExecutionException(
+            "An unexpected exception occured when running SJS compilation.", ex);
+      } catch (ScriptException ex) {
+        new MojoExecutionException(
+            "An unexpected exception occured when running SJS compilation.", ex);
+      }
+    } else {
+      getLog().info("No change detected. Skipping generation.");
+    }
+    project.addCompileSourceRoot(outputDir.getPath());
+  }
+
+  private void runSjsCompilation() throws IOException, ResourceException,
+      ScriptException {
+    Properties projectProperties = project.getProperties();
+    projectProperties.put("srcDir", srcDir.getAbsolutePath());
+    projectProperties.put("outputDir", outputDir.getAbsolutePath());
+    projectProperties.put("modelOutputFileName", modelOutputFileName);
+    projectProperties.put("viewOutputFileName", viewOutputFileName);
+    projectProperties.put("backOutputFileName", backOutputFileName);
+    projectProperties.put("frontOutputFileName", frontOutputFileName);
+    GroovyScriptEngine gse = new GroovyScriptEngine(new URL[] {
+      srcDir.toURI().toURL()
+    });
+    Binding binding = new Binding();
+    binding.setVariable("project", project);
+    gse.run(applicationFileName, binding);
+  }
+
+  private boolean isChangeDetected() {
+    if (!outputDir.exists()) {
+      return true;
+    }
+    long outputLastModified = latestModified(outputDir,
+        outputDir.lastModified());
+    getLog().info("Scanning for changes : " + srcDir);
+    if (hasChangedSourceFile(srcDir, outputLastModified)) {
+      return true;
+    }
+    return false;
+  }
+
+  private long latestModified(File root, long maxLastModified) {
+    long latest = maxLastModified;
+    if (root.lastModified() > maxLastModified) {
+      latest = root.lastModified();
+    }
+    if (root.isDirectory()) {
+      for (File child : root.listFiles()) {
+        latest = latestModified(child, latest);
+      }
+    }
+    return latest;
+  }
+
+  private boolean hasChangedSourceFile(File source, long maxLastModified) {
+    if (source.isDirectory()) {
+      for (File childSource : source.listFiles()) {
+        if (hasChangedSourceFile(childSource, maxLastModified)) {
+          return true;
+        }
+      }
+    } else {
+      if (source.lastModified() > maxLastModified) {
+        getLog().info(
+            "Detected a change on source " + source.toString() + ". "
+                + new Date(source.lastModified()) + " > "
+                + new Date(maxLastModified));
+        return true;
+      }
+    }
+    return false;
+  }
+}
