0419a441dc9f857dc1aa0da152e82b950759f1f4
Vincent Vandenschrick <vvandens@jspresso.org>
2013-06-12 15:31:18 +0000

    1059 Properties starting with a single lowercase letter are falsely marked dirty when the entity is hydrated

--- hibernate/src/main/java/org/jspresso/framework/application/backend/persistence/hibernate/ControllerAwareEntityProxyInterceptor.java
+++ hibernate/src/main/java/org/jspresso/framework/application/backend/persistence/hibernate/ControllerAwareEntityProxyInterceptor.java
@@ -145,7 +145,7 @@ public class ControllerAwareEntityProxyInterceptor extends
       if (backendController.isUnitOfWorkActive()) {
         Map<String, Object> properties = new HashMap<String, Object>();
         for (int i = 0; i < propertyNames.length; i++) {
-          String propertyName = propertyNames[i];
+          String propertyName = AbstractPropertyAccessor.fromJavaBeanPropertyName(propertyNames[i]);
           if (!isHibernateInternal(propertyName)) {
             properties.put(propertyName, state[i]);
           }
@@ -160,7 +160,7 @@ public class ControllerAwareEntityProxyInterceptor extends
           Map<String, Object> properties = new HashMap<String, Object>();
           for (int i = 0; i < propertyNames.length; i++) {
             if (state[i] != null) {
-              String propertyName = propertyNames[i];
+              String propertyName = AbstractPropertyAccessor.fromJavaBeanPropertyName(propertyNames[i]);
               if (!isHibernateInternal(propertyName)) {
                 if (state[i] instanceof IEntity) {
                   IEntity refEntity = (IEntity) state[i];
