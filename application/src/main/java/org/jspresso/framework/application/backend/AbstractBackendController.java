588c4ac26e2210d691b0b77782271c9451d45392
Vincent Vandenschrick <vvandens@jspresso.org>
2013-11-16 12:30:47 +0000

    Backport #1134

--- application/src/main/java/org/jspresso/framework/application/backend/AbstractBackendController.java
+++ application/src/main/java/org/jspresso/framework/application/backend/AbstractBackendController.java
@@ -1453,7 +1453,7 @@ public abstract class AbstractBackendController extends AbstractController
       boolean newlyRegistered = false;
       if (registeredEntity == null) {
         if (!isInitialized(entity)) {
-          return entity;
+          return mergeUninitializedEntity(entity);
         }
         registeredEntity = carbonEntityCloneFactory.cloneEntity(entity,
             entityFactory);
@@ -1597,6 +1597,17 @@ public abstract class AbstractBackendController extends AbstractController
       }
       setDirtyTrackingEnabled(dirtRecorderWasEnabled);
     }
+  }
+
+  /**
+   * Merge non initialized entity.
+   *
+   * @param <E> the actual entity type
+   * @param entity the entity
+   * @return the merged entity
+   */
+  protected <E extends IEntity> E mergeUninitializedEntity(E entity) {
+    return entity;
   }
 
   /**
