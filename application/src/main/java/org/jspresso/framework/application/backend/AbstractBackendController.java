3ecafc689549401accbc6e9c102b2f814f37faac
Vincent Vandenschrick <vvandens@jspresso.org>
2013-11-29 08:25:08 +0000

    1137 Allow to have async actions to register on completion callbacks

--- application/src/main/java/org/jspresso/framework/application/backend/AbstractBackendController.java
+++ application/src/main/java/org/jspresso/framework/application/backend/AbstractBackendController.java
@@ -151,6 +151,7 @@ public abstract class AbstractBackendController extends AbstractController
   private       boolean                                throwExceptionOnBadUsage;
   private       IBackendControllerFactory              slaveControllerFactory;
   private       int                                    asyncExecutorsMaxCount;
+  private       IBackendController                     masterController;
 
   /**
    * Constructs a new {@code AbstractBackendController} instance.
@@ -398,6 +399,7 @@ public abstract class AbstractBackendController extends AbstractController
     slaveBackendController.start(getLocale(), getClientTimeZone());
     // Use the same application session
     slaveBackendController.setApplicationSession(getApplicationSession());
+    slaveBackendController.masterController = this;
     return slaveBackendController;
   }
 
@@ -2621,5 +2623,20 @@ public abstract class AbstractBackendController extends AbstractController
    */
   protected void resumeUnitOfWork() {
     unitOfWork.resume();
+  }
+
+  /**
+   * Delegates to the master controller if any.
+   *
+   * @param action the action
+   * @param context the context
+   */
+  @Override
+  public synchronized void executeLater(IAction action, Map<String, Object> context) {
+    if (masterController != null) {
+      masterController.executeLater(action, context);
+    } else {
+      super.executeLater(action, context);
+    }
   }
 }
