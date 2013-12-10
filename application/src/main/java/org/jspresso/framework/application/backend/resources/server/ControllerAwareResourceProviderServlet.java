88e74044e8d77f78d649061111bfefcffa87a57e
Vincent Vandenschrick <vvandens@jspresso.org>
2013-12-10 09:09:45 +0000

    Fixed NPE in async export active resource

--- application/src/main/java/org/jspresso/framework/application/backend/resources/server/ControllerAwareResourceProviderServlet.java
+++ application/src/main/java/org/jspresso/framework/application/backend/resources/server/ControllerAwareResourceProviderServlet.java
@@ -23,8 +23,6 @@ import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.jspresso.framework.action.IActionHandler;
 import org.jspresso.framework.application.backend.AbstractBackendController;
 import org.jspresso.framework.application.backend.BackendControllerHolder;
