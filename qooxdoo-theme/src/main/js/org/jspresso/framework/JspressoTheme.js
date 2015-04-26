aa84127969dc13824942d7d406c73d6ef71315ab
Vincent Vandenschrick <vvandens@gmail.com>
2015-04-26 11:21:01 +0200

    jspresso-ce-17 Rework the Jspresso Qooxdoo theme : Theme naming

---
+++ qooxdoo-theme/src/main/js/org/jspresso/framework/JspressoTheme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("org.jspresso.framework.JspressoTheme",
+{
+  meta :
+  {
+    color : org.jspresso.framework.theme.Color,
+    decoration : org.jspresso.framework.theme.Decoration,
+    font : org.jspresso.framework.theme.Font,
+    icon : qx.theme.icon.Oxygen,
+    appearance : org.jspresso.framework.theme.Appearance
+  }
+});
