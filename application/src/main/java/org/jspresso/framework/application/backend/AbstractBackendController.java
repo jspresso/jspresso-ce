8193adfccb3fde17401a678b185171e525c9ddbf
Vincent Vandenschrick <vvandens@gmail.com>
2015-05-04 18:00:25 +0200

    TX UOW should not be used when not inside a TX. This regression was introduced with 4.1-SNAP

--- application/src/main/java/org/jspresso/framework/application/backend/AbstractBackendController.java
+++ application/src/main/java/org/jspresso/framework/application/backend/AbstractBackendController.java
@@ -212,6 +212,7 @@ public abstract class AbstractBackendController extends AbstractController imple
         registerForDeletion(uowEntity);
       }
     }
+    clearPendingOperations();
   }
 
   /**
