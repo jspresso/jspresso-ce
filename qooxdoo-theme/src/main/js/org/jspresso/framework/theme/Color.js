77ba6cb5a3c0eea4f412ba5698840cfd4c974fe0
Vincent Vandenschrick <vvandens@gmail.com>
2015-05-26 10:35:01 +0200

    jspresso-ce-17 Rework the Jspresso Qooxdoo theme : colors (cherry picked from commit ae6b926)

--- qooxdoo-theme/src/main/js/org/jspresso/framework/theme/Color.js
+++ qooxdoo-theme/src/main/js/org/jspresso/framework/theme/Color.js
@@ -17,49 +17,52 @@ qx.Theme.define("org.jspresso.framework.theme.Color",
       extend: qx.theme.simple.Color,
 
       colors: {
+        "dark-blue": "#44509E",
+        "light-blue": "#6694E3",
+
+        "app-foreground": "dark-blue",
+        "app-background": "white",
+        "text-selected": "white",
+
+        "background-selected": "light-blue",
+        "background-selected-dark": "app-foreground",
+        "background-selected-dark-disabled": "background-selected-disabled",
+
         "border-separator": "#CCCCCC",
 
         "button-border": "#CCCCCC",
         "button-border-hovered": "#BBBBBB",
 
-        "table-row": "gray",
+        "label": "#777777",
+
+        "table-row": "label",
         "table-row-selected": "text-selected",
         "table-row-background-even": "white",
         "table-row-background-odd": "light-background",
+        "table-row-background-selected": "background-selected-dark",
+        "table-row-background-focused-selected": "table-row-background-selected",
+
+        "table-focus-indicator": "background-selected",
+        "border-lead": "background-selected",
 
         "border-main": "border-light",
 
         "header": "white",
-        "header-text": "gray",
-        "header-text-selected": "gray",
+        "header-text": "label",
+        "header-text-selected": "label",
         "light-background": "#F2F2F2",
         "dark-background": "#E2E2E2",
 
-        "app-foreground": "#4568BB",
-        "app-background": "white",
-        "text-selected": "white",
+        "section-header-text": "app-foreground",
 
         "window-caption-background": "light-background",
         "window-border": "#979797",
         "window-border-inner": "#B7B7B7",
 
-        "background-selected": "#6694E3",
-        "background-selected-dark": "app-foreground",
-        "background-selected-dark-disabled": "background-selected-disabled",
-
-        "table-focus-indicator": "background-selected",
-        "border-lead": "background-selected",
-
-        "table-row-background-focused-selected": "background-selected-dark",
-        "table-row-background-focused": "background-selected-dark",
-        "table-row-background-selected": "background-selected-dark",
-
         "tooltip": "light-background",
-        "tooltip-text": "gray",
+        "tooltip-text": "label",
 
-        "label": "gray",
-
-        "navigator": "background-selected-dark",
+        "navigator": "app-foreground",
         "navigator-text": "white",
         "navigator-header": "navigator",
         "navigator-header-text": "white",
