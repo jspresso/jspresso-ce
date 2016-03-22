f643c133315fbfc63e0287962dab36a9b111b2a3
Vincent Vandenschrick <vvandens@gmail.com>
2016-03-22 08:34:46 +0100

    [HTML5] Combobox style (cherry picked from commit daf2216)

--- qooxdoo-theme/src/main/js/org/jspresso/framework/theme/Appearance.js
+++ qooxdoo-theme/src/main/js/org/jspresso/framework/theme/Appearance.js
@@ -130,7 +130,8 @@ qx.Theme.define("org.jspresso.framework.theme.Appearance",
           include: "button-frame",
           style: function (states) {
             return {
-              padding: [2, 4]
+              padding: [2, 4],
+              margin: 2
             };
           }
         },
