daf22164e902ccba05e79c51e2af9a2cb7876cf9
Vincent Vandenschrick <vvandens@gmail.com>
2016-03-22 08:34:46 +0100

    [HTML5] Combobox style

--- qooxdoo-theme/src/main/js/org/jspresso/framework/theme/Appearance.js
+++ qooxdoo-theme/src/main/js/org/jspresso/framework/theme/Appearance.js
@@ -129,7 +129,8 @@ qx.Theme.define("org.jspresso.framework.theme.Appearance",
           include: "button-frame",
           style: function (states) {
             return {
-              padding: [2, 4]
+              padding: [2, 4],
+              margin: 2
             };
           }
         },
