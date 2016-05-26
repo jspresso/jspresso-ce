e96d5905d26868819b32cf8c134d222fc873c82f
Vincent Vandenschrick <vvandens@gmail.com>
2016-05-26 18:50:15 +0200

    jspresso-ce-120 ViewDescriptor: "Repeatable" descriptor fixes #120

--- qooxdoo/client/src/main/js/org/jspresso/framework/view/qx/DefaultQxViewFactory.js
+++ qooxdoo/client/src/main/js/org/jspresso/framework/view/qx/DefaultQxViewFactory.js
@@ -2748,7 +2748,6 @@ qx.Class.define("org.jspresso.framework.view.qx.DefaultQxViewFactory", {
       }
 
       splitContainer.addListenerOnce("appear", function (appearEvent) {
-        var ltComponent, rbComponent, wrapper;
         var ltSize, rbSize, splitSize;
         var wrapper;
         if (ltComponent) {
@@ -2896,7 +2895,7 @@ qx.Class.define("org.jspresso.framework.view.qx.DefaultQxViewFactory", {
           this._getActionHandler());
       repeater.setDataProvider(remoteRepeater.getState().getChildren());
       remoteRepeater.assignPeer(repeater);
-      
+
       var scroller = new qx.ui.container.Scroll();
       scroller.setScrollbarX("off");
       scroller.setScrollbarY("auto");
