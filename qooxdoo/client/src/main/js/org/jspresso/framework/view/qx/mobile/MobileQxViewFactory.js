e88db9cb29ae4c81c65dbe48396246b2a7398924
Vincent Vandenschrick <vvandens@gmail.com>
2016-06-13 11:48:55 +0200

    jspresso-ce-146 Two lines of text for mobile action

--- qooxdoo/client/src/main/js/org/jspresso/framework/view/qx/mobile/MobileQxViewFactory.js
+++ qooxdoo/client/src/main/js/org/jspresso/framework/view/qx/mobile/MobileQxViewFactory.js
@@ -240,6 +240,22 @@ qx.Class.define("org.jspresso.framework.view.qx.mobile.MobileQxViewFactory", {
         }
         toolBar.add(actionComponent);
       }
+      toolBar.addListener("appear", function (e) {
+        qx.event.Timer.once(function () {
+          var maxH = 0;
+          var children = toolBar.getChildren();
+          for (var childI = 0; childI < children.length; childI++) {
+            if (children[childI].getContainerElement().clientHeight > maxH) {
+              maxH = children[childI].getContainerElement().offsetHeight;
+            }
+          }
+          if (maxH > 0) {
+            for (childI = 0; childI < children.length; childI++) {
+              children[childI]._setStyle("height", maxH + "px");
+            }
+          }
+        }, this, 50);
+      }, this);
       return toolBar;
     },
 
@@ -894,6 +910,7 @@ qx.Class.define("org.jspresso.framework.view.qx.mobile.MobileQxViewFactory", {
         button.setShow("both");
       } else if (label) {
         button.setShow("label");
+        button.getLabelWidget().setWrap(true);
       } else {
         //button.setGap(0);
         //button.removeCssClass("gap");
