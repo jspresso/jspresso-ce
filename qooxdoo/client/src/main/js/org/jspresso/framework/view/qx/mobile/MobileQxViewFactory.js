33035199a6a681b2c28a4b0247442369725882be
Vincent Vandenschrick <vvandens@gmail.com>
2016-10-19 16:43:09 +0200

    jspresso-ce-199 Mobile Image should scale to available width fixes #199

--- qooxdoo/client/src/main/js/org/jspresso/framework/view/qx/mobile/MobileQxViewFactory.js
+++ qooxdoo/client/src/main/js/org/jspresso/framework/view/qx/mobile/MobileQxViewFactory.js
@@ -2078,6 +2078,7 @@ qx.Class.define("org.jspresso.framework.view.qx.mobile.MobileQxViewFactory", {
      */
     _createImageComponent: function (remoteImageComponent) {
       var imageComponent = new qx.ui.mobile.basic.Image();
+      qx.bom.element.Style.set(imageComponent.getContentElement(), "max-width", "100%");
 
       var state = remoteImageComponent.getState();
       state.addListener("changeValue", function (e) {
