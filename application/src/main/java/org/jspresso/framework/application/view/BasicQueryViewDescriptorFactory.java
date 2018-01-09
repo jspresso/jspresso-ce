dba2507b1477c13741d321c1c7cb5c900c74a4a5
Vincent Vandenschrick <vvandens@gmail.com>
2018-01-09 11:50:56 +0100

    Filter view : comparable query structures should always be not resizable forms

--- application/src/main/java/org/jspresso/framework/application/view/BasicQueryViewDescriptorFactory.java
+++ application/src/main/java/org/jspresso/framework/application/view/BasicQueryViewDescriptorFactory.java
@@ -217,7 +217,7 @@ public class BasicQueryViewDescriptorFactory<E, F, G> implements IQueryViewDescr
                                                                                      IAction propertyViewAction) {
     BasicComponentViewDescriptor comparableComponentViewDescriptor = new BasicComponentViewDescriptor();
     comparableComponentViewDescriptor.setLabelsPosition(ELabelPosition.NONE);
-    //comparableComponentViewDescriptor.setWidthResizeable(false);
+    comparableComponentViewDescriptor.setWidthResizeable(false);
     comparableComponentViewDescriptor.setColumnCount(3);
     comparableComponentViewDescriptor.setModelDescriptor(
         ((ComparableQueryStructureDescriptor) actualPropertyDescriptor).getReferencedDescriptor());
